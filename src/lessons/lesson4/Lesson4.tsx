291a03bdb8b9d5687603e30f261de34dff1a66d9
Crash2022 <crash555@mail.ru>
2022-10-28 12:44:44 +0500

    lesson 4 task 5 done

--- src/lessons/lesson4/Lesson4.tsx
+++ src/lessons/lesson4/Lesson4.tsx
@@ -1,34 +1,45 @@
 import React from 'react'
 import './lesson_4';
 
+const handlePromise: any = {
+    promise: null,
+    resolve: null,
+    reject: null,
+    onSuccess: function (paramName: any) {
+        console.log(`Promise is resolved with data: ${paramName}`);
+    },
+    onError: function (paramName: any) {
+        console.log(`Promise is rejected with error: ${paramName}`);
+    }
+}
+
 export const Lesson4 = () => {
 
-    const handlePromise = {
-        promise: null,
-        resolve: null,
-        reject: null,
-        onSuccess: function(paramName: any) {
-            console.log(`Promise is resolved with data: ${paramName}`);
-        },
-        onError: function(paramName: any) {
-            console.log(`Promise is rejected with error: ${paramName}`);
-        }
-    }
-
     const createPromise = () => {
-        return new Promise((res, rej) => {
-
-        })
+        const promise = new Promise((resolve, reject) => {
+            handlePromise.resolve = resolve;
+            handlePromise.reject = reject;
+        });
+        handlePromise.promise = promise;
+    }
+    const resolvePromise = () => {
+        handlePromise.resolve('RESOLVED');
+        handlePromise.promise.then((data: any) => handlePromise.onSuccess(data));
+    }
+    const rejectPromise = () => {
+        handlePromise.reject('REJECTED');
+        handlePromise.promise.catch((data: any) => handlePromise.onError(data));
     }
 
     return (
-        <div style={{marginBottom: '30px',
+        <div style={{
+            marginBottom: '30px',
             border: '1px solid black',
             padding: '10px', marginTop: '10px'
         }}>
             <button id='btn-create-promise' onClick={createPromise}>Create Promise</button>
-            <button id='btn-resolve-promise'>Resolve Promise</button>
-            <button id='btn-reject-promise'>Reject Promise</button>
+            <button id='btn-resolve-promise' onClick={resolvePromise}>Resolve Promise</button>
+            <button id='btn-reject-promise' onClick={rejectPromise}>Reject Promise</button>
         </div>
     );
 }
